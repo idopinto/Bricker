192ce8fd1f73f6b53bef694e801b2472aec580d5
idopinto <ido.pinto@mail.huji.ac.il>
2021-11-12 23:59:00 +0200

    add Brick.java

--- src/bricker/gameobjects/UserPaddle.java
+++ src/bricker/gameobjects/UserPaddle.java
@@ -1,11 +1,65 @@
 package bricker.gameobjects;
 
 import danogl.GameObject;
+import danogl.gui.UserInputListener;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
+import java.awt.event.KeyEvent;
+import java.util.Vector;
+
 public class UserPaddle extends GameObject {
-    public UserPaddle(Vector2 zero, Vector2 vector2, Renderable paddleImage) {
-        super();
+
+    private static final float MOVEMENT_SPEED = 300;
+    private static final int MIN_DISTANCE_FROM_SCREEN_EDGE = 10;
+
+    private UserInputListener inputListener;
+    private float windowWidth;
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        Vector2 movementDirection = Vector2.ZERO;
+        movementDirection = setMovementDirection(movementDirection);
+        setVelocity(movementDirection.mult(MOVEMENT_SPEED));
+        validatePaddleBoundaries();
+    }
+
+    private void validatePaddleBoundaries()
+    {
+        if ((MIN_DISTANCE_FROM_SCREEN_EDGE > getTopLeftCorner().x()))
+        {
+            transform().setTopLeftCornerX(MIN_DISTANCE_FROM_SCREEN_EDGE);
+        }
+        if ((getTopLeftCorner().x() > windowWidth - MIN_DISTANCE_FROM_SCREEN_EDGE - getDimensions().x()))
+        {
+            transform().setTopLeftCornerX(windowWidth - MIN_DISTANCE_FROM_SCREEN_EDGE - getDimensions().x());
+        }
+    }
+
+    private Vector2 setMovementDirection(Vector2 movementDirection)
+    {
+        if (inputListener.isKeyPressed(KeyEvent.VK_LEFT))
+        {
+            movementDirection = movementDirection.add(Vector2.LEFT);
+        }
+        if (inputListener.isKeyPressed(KeyEvent.VK_RIGHT))
+        {
+            movementDirection = movementDirection.add(Vector2.RIGHT);
+        }
+        return movementDirection;
+    }
+    /**
+     * Construct a new GameObject instance.
+     *  @param topLeftCorner Position of the object, in window coordinates (pixels).
+     *                      Note that (0,0) is the top-left corner of the window.
+     * @param dimensions    Width and height in window coordinates.
+     * @param renderable    The renderable representing the object. Can be null, in which case
+     * @param inputListener
+     */
+    public UserPaddle(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, UserInputListener inputListener, float windowWidth) {
+        super(topLeftCorner, dimensions, renderable);
+        this.inputListener = inputListener;
+        this.windowWidth = windowWidth;
     }
 }
--- src/bricker/main/BrickerGameManager.java
+++ src/bricker/main/BrickerGameManager.java
@@ -12,9 +12,16 @@ import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
 import java.awt.*;
+import java.util.Random;
 
 public class BrickerGameManager extends GameManager {
-    private static final float BORDER_WIDTH = 10;
+    private static final int WINDOW_WIDTH= 700;
+    private static final int WINDOW_HEIGHT = 500;
+    private static final int BORDER_WIDTH = 10;
+    private static final int PADDLE_HEIGHT = 15;
+    private static final int PADDLE_WIDTH = 100;
+    private static final int BALL_RADIUS = 20;
+    private static final float BALL_SPEED = 300;
     private static final Color BORDER_COLOR = Color.BLACK;
 
     BrickerGameManager(String windowTitle, Vector2 windowDimensions) {
@@ -27,30 +34,44 @@ public class BrickerGameManager extends GameManager {
         Vector2 windowDimensions = windowController.getWindowDimensions();
         initializeBackground(windowDimensions,imageReader);
         createBall(imageReader,soundReader,windowDimensions);
-        createPaddles(imageReader,windowDimensions);
+        createPaddles(imageReader,inputListener,windowDimensions);
         createBorders(windowDimensions);
     }
 
     private void createBall(ImageReader imageReader,SoundReader soundReader, Vector2 windowDimensions) {
         Renderable ballImage = imageReader.readImage("assets/ball.png", true);
         Sound collisionSound = soundReader.readSound("assets/blop.wav");
-        GameObject ball = new Ball(Vector2.ZERO, new Vector2(20, 20), ballImage,collisionSound);
-        ball.setVelocity(Vector2.DOWN.mult(300));
+        GameObject ball = new Ball(Vector2.ZERO, new Vector2(BALL_RADIUS, BALL_RADIUS), ballImage,collisionSound);
+        ball.setVelocity(reflectVelocityVectorRandomly());
         ball.setCenter(windowDimensions.mult(0.5f));
         this.gameObjects().addGameObject(ball);
     }
 
-    private void createPaddles(ImageReader imageReader, Vector2 windowDimensions)
+    private Vector2 reflectVelocityVectorRandomly()
+    {
+        float ballVelX = BALL_SPEED;
+        float ballVelY = BALL_SPEED;
+        Random random = new Random();
+        if (random.nextBoolean()){
+            ballVelX *= -1;
+        }
+        else {
+            ballVelY *= -1;
+        }
+        return new Vector2(ballVelX,ballVelY);
+    }
+
+    private void createPaddles(ImageReader imageReader, UserInputListener inputListener, Vector2 windowDimensions)
     {
         Renderable paddleImage = imageReader.readImage("assets/paddle.png",true);
 
         // create user paddle
-        GameObject userPaddle = new UserPaddle(Vector2.ZERO, new Vector2(100, 15), paddleImage);
+        GameObject userPaddle = new UserPaddle(Vector2.ZERO, new Vector2(PADDLE_WIDTH, PADDLE_HEIGHT), paddleImage,inputListener,windowDimensions.x());
         userPaddle.setCenter(new Vector2(windowDimensions.x() / 2,(int)windowDimensions.y() - 30));
         this.gameObjects().addGameObject(userPaddle);
 
         // create AI paddle
-        GameObject aiPaddle = new GameObject(Vector2.ZERO, new Vector2(100, 15), paddleImage);
+        GameObject aiPaddle = new GameObject(Vector2.ZERO, new Vector2(PADDLE_WIDTH, PADDLE_HEIGHT), paddleImage);
         aiPaddle.setCenter(new Vector2(windowDimensions.x() / 2,30));
         this.gameObjects().addGameObject(aiPaddle);
 
@@ -72,7 +93,7 @@ public class BrickerGameManager extends GameManager {
 
     }
     public static void main(String[] args) {
-        Vector2 v = new Vector2(700, 500);
+        Vector2 v = new Vector2(WINDOW_WIDTH, WINDOW_HEIGHT);
         BrickerGameManager a = new BrickerGameManager("Bricker", v);
         a.run();
     }
